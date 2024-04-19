3c6e93fdcfacd021f25bd7118bc211bf9dc27068
Ominai <104097942+Ominai@users.noreply.github.com>
2024-04-18 19:54:02 -0700

    npm update

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ import './style.css';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-// Testing Push
 // Setup
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
