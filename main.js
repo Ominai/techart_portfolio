468d000c443193ba5cb76c650809042c40313bcb
kaida <LeAnh.Nguyen@proton.me>
2024-04-20 18:50:35 -0700

    Updates

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './style.css';
 import * as THREE from 'three';
-import {OBJLoader} from 'three/addons/loaders/OBJLoader.js';
+import {GLTFLoader} from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 // Setup
@@ -10,28 +10,20 @@ const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
 });
 
+// Camera and Rendering
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 camera.position.setX(-3);
 renderer.render(scene, camera);
 
-// Fembur OBJ
-{
-    const objLoader = new OBJLoader();
-    objLoader.load('Fembur.obj', (root) => {
-      scene.add(root);
-    });
-}
-
-// Shapes
-const torusgeometry = new THREE.TorusGeometry(12, 1, 32, 160);
-const torusmaterial = new THREE.MeshStandardMaterial({ color: 0xdaa520 });
-const torus = new THREE.Mesh(torusgeometry, torusmaterial);
-const knotgeometry = new THREE.TorusKnotGeometry( 3, 1.5, 150, 18, 2, 7 ); 
-const knotmaterial = new THREE.MeshStandardMaterial( { color: 0xdaa520 } ); 
-const torusKnot = new THREE.Mesh( knotgeometry, knotmaterial ); 
-scene.add( torus );
+// gltf
+const gltfLoader = new GLTFLoader();
+const url = 'scene.gltf';
+gltfLoader.load(url, (gltf) => {
+  const root = gltf.scene;
+  scene.add(root);
+});
 
 // Lighting
 const pointLight = new THREE.PointLight( 0x0000ff, 200000, 0, 2.2 );
@@ -40,10 +32,9 @@ const ambientLight = new THREE.AmbientLight( 0xfff000, 1 );
 scene.add( pointLight, ambientLight );
 
 // Helpers
-//const lightHelper = new THREE.PointLightHelper(pointLight)
-//const gridHelper = new THREE.GridHelper(200, 50);
-//scene.add(lightHelper, gridHelper)
-
+const lightHelper = new THREE.PointLightHelper(pointLight)
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(lightHelper, gridHelper)
 const controls = new OrbitControls(camera, renderer.domElement);
 
 // Stars
@@ -57,36 +48,14 @@ function addStar() {
     star.position.set(x, y, z);
     scene.add(star)
 }
-
 Array(200).fill().forEach(addStar)
-
 const spaceTexture = new THREE.TextureLoader().load('space.jpg');
 scene.background = spaceTexture;
-
-// Avatar and Box
-const ravinTexture = new THREE.TextureLoader().load('avatar.png');
-const ravin = new THREE.Mesh(
-    new THREE.BoxGeometry(3,3,3),
-    new THREE.MeshBasicMaterial( { map: ravinTexture } )
-);
-scene.add(ravin);
 
 // Animation Loop
 function animate() {
     requestAnimationFrame(animate);
-    torus.rotation.x += 0.01;
-    torus.rotation.y += 0.005;
-    torus.rotation.z += 0.01;
-    torusKnot.rotation.x -= 0.01;
-    torusKnot.rotation.y -= 0.01;
-    torusKnot.rotation.z -= 0.01;
-
-    ravin.rotation.x -= 0.01;
-    ravin.rotation.y -= 0.01;
-    ravin.rotation.z -= 0.01;
-
     controls.update();
-
     renderer.render(scene, camera);
 }
 
