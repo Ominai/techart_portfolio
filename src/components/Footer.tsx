3cf5a9b4ee9eed1fb2c4862dbf07e73b8899f616
Ominai <104097942+Ominai@users.noreply.github.com>
2025-08-04 19:02:44 -0700

    Syntax corrections

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -17,7 +17,7 @@ export const Footer = () => {
             justifyContent="center" mobileDirection="column">
             <Flex
                 className={styles.mobile}
-                fillWidth maxWidth="m" padding"8" paddingX="16" gap="16"
+                fillWidth maxWidth="m" paddingY="8" paddingX="16" gap="16"
                 justifyContent="space-between" alignItems="center">
                 <Text
                     variant="body-default-s"
@@ -28,7 +28,7 @@ export const Footer = () => {
                     </Text>
                     <Text paddingX="4">
                         {person.name}
-                    </Text
+                    </Text>
                 </Text>
                 <Flex
                     gap="16">
